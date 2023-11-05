4c27e64159f5431d3c97dd402bf2223e86fede6a
alemranakash <alemranexpert@gmail.com>
2023-11-05 15:18:44 +0600

    Added comment functionality to the Blog Details Page

--- src/Pages/Blogs/BlogDetails.jsx
+++ src/Pages/Blogs/BlogDetails.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import  { useContext, useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../Provider/AuthProvider";
-
+import axios from 'axios';
 
 const BlogDetails = () => {
     const {user}= useContext(AuthContext)
@@ -23,13 +23,53 @@ const BlogDetails = () => {
 
 console.log(blogDetails);
 
-  const { title, image, short_description, long_description, category, blogOwnerEmail  } = blogDetails;
+  const { title, image, short_description, long_description, category, blogOwnerEmail, _id  } = blogDetails;
   const isOwner = userEmail === blogOwnerEmail;
- 
+
+
+
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const comment = e.target.comment.value;
+    console.log(comment);
+
+    const addComment= {
+        comment, _id, commentOwnerName: user.displayName, commentOwnerPhoto: user.photoURL
+    }
+    console.log(addComment);
+
+    axios
+    .post('http://localhost:5000/allComments', addComment, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+
+     
+      if (response.data.insertedId) {
+        swal({
+          title: 'Comment Added',
+          text: 'Comment added successfully',
+          icon: 'success',
+        });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      
+    });
+
+
+  }
+
+
   return (
     <div>
       <h1 className="text-4xl text-center">Blog Details</h1>
-      <div className="card lg:card-side bg-blue-200 shadow-xl">
+      <div className="card lg:card-side bg-blue-200 shadow-xl my-20">
         <figure className="p-4">
           <img className="rounded-lg w-full h-full" src={image} alt="Blog" />
         </figure>
@@ -47,6 +87,36 @@ console.log(blogDetails);
           )}
         </div>
       </div>
+
+<div>
+<div className="mb-4">
+<form onSubmit={handleFormSubmit} className="bg-blue-100 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <label className="block text-gray-700 text-2xl font-bold mb-4" htmlFor="comment">
+       Add your Comment here:
+      </label>
+      <textarea
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id="comment"
+        placeholder="Comments"
+        name="comment"
+        rows="3"
+      ></textarea>
+      <div className="flex items-center justify-between">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="submit"
+      >
+        Comment
+      </button>
+    </div>
+       </form>
+    </div>
+</div>
+
+
+
+
+
     </div>
   );
 };
